ce17389194f79eaf4b93b8d3cacd6411da213091
Lukas <toasterpanic@gmail.com>
2023-05-11 15:09:35 +0000

    Update file os.js

--- assets/scripts/os.js
+++ assets/scripts/os.js
@@ -806,11 +806,11 @@ function checkFinder(str) {
   for (let i=0; i<match.length&&i<12;) {
     var div = document.createElement("div");
     div.innerHTML = match[i].name
-    div.onclick = function() {
+    div.addEventListener("keydown", function() {
       finder.style = "display: none;"
       finderBox.style = "display: none;"
-      eval(match[i].onclick);
-    }
+    })
+    div.addEventListener("keydown", match[i].onclick)
     finderBox.appendChild(div);
     ++i;
   }
