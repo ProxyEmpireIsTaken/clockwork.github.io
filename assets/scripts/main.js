2369d02bffebe8609fb5c072285523db04adaf5f
Lukas <toasterpanic@gmail.com>
2023-05-15 22:48:26 +0000

    Update 2 files
    - /assets/scripts/main.js
    - /get-started/index.html

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,19 @@ document.querySelector(".navbar").innerHTML = `<a href="https://discord.gg/Sb8Nz
 
 if (document.location.pathname.startsWith("/get-started")) {
   const url = "https://"+document.location.hostname+"/os/"
+  const htmlPage = `
+  <!DOCTYPE html>
+  <html>
+  <body>
+  <iframe src="${btoa(url)}"></iframe>
+  <style>
+  * {margin: 0; padding: 0;}iframe {width: 100%;height: 100vh;border: none;}
+  </style>
+  <script>
+  document.querySelector("iframe").src = atob(document.querySelector("iframe").src);
+  </script>
+  </body>
+  </html>\`)`
   const aboutBlanker = `javascript:
   var win = window.open("","_blank","popup=yes");
   win.location.origin = 'https://google.com';
@@ -17,4 +30,5 @@ if (document.location.pathname.startsWith("/get-started")) {
   </body>
   </html>\`);`
   document.querySelector("#blanker").href = aboutBlanker;
+  document.querySelector("#data").href = "data:text/html;base64,"+btoa(url);
 }
