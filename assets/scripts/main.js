4215ed37224e3d0bafbdfaaa53a50e4ece40316b
Lukas <toasterpanic@gmail.com>
2023-05-15 22:14:51 +0000

    Update file main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,7 +7,7 @@ if (document.location.pathname.startsWith("/get-started")) {
   const aboutBlanker = `javascript:
   var win = window.open("","_blank","popup=yes");
   win.location.origin = 'https://google.com';
-  win.document.body.innerHTML = \`<!DOCTYPE html>
+  win.document.write(\`<!DOCTYPE html>
   <html>
   <body>
   <iframe src="\${atob('${btoa(url)}')}"></iframe>
@@ -15,7 +15,7 @@ if (document.location.pathname.startsWith("/get-started")) {
   * {margin: 0; padding: 0;}iframe {width: 100%;height: 100vh;border: none;}
   </style>
   </body>
-  </html>\`;
+  </html>\`);
   document.location.reload();`
   document.querySelector("#blanker").href = aboutBlanker;
 }
