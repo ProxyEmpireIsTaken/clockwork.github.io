4b85302245b3945d0bfcbb0f0e9400d491e3255b
Lukas <toasterpanic@gmail.com>
2023-05-15 22:00:09 +0000

    Update 3 files
    - /assets/scripts/main.js
    - /get-started/index.html
    - /index.html

---
+++ assets/scripts/main.js
@@ -0,0 +1,19 @@
+/* Used across the Clockwork main pages */
+document.querySelector(".navbar").innerHTML = `<a href="https://discord.gg/Sb8NzVbqX8">Discord</a> | 
+© 2023 <a href="https://redstone-network.vercel.app/">Redstone Network</a>`
+
+if (document.location.pathname.startsWith("/get-started")) {
+  const url = "https://"+document.location.hostname+"/os/"
+  const aboutBlanker = `javascript:
+  var win=window.open("","_blank","popup=yes");
+  win.document.body.innerHTML = \`<!DOCTYPE html>
+  <html>
+  <body>
+  <iframe src=\${atob(${btoa(url)})}></iframe>
+  <style>
+  * {margin: 0; padding: 0;}iframe {width: 100%;height: 100vh;border: none;}
+  </style>
+  </body>
+  </html>\`;`
+  document.querySelector("#blanker").innerHTML = aboutBlanker;
+}
