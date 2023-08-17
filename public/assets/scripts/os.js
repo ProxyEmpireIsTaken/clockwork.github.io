33ead04e5cdd0832913fe14aa0f8dc48f2ed8507
Lukas <toasterpanic@gmail.com>
2023-08-17 22:54:49 +0000

    Update file os.js

--- public/assets/scripts/os.js
+++ public/assets/scripts/os.js
@@ -412,8 +412,8 @@ function setupScreenSwap(screen) {
   var children = document.querySelectorAll("div.setup-screen");
   let i = 0;
   while (i != children.length) {
-    if (children[i].id == "setup-screen-"+screen) children[i].style.className = "setup-screen visible"
-    else children[i].style.className = "setup-screen";
+    if (children[i].id == "setup-screen-"+screen) children[i].className = "setup-screen visible"
+    else children[i].className = "setup-screen";
     i++;
   }
 }
