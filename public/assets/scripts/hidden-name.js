4206bfb23bf7c3871f2c2c57ebd1d6a9f2d5fe87
Lukas <toasterpanic@gmail.com>
2023-08-18 13:05:19 +0000

    Update file hidden-name.js

--- public/assets/scripts/hidden-name.js
+++ public/assets/scripts/hidden-name.js
@@ -9,5 +9,5 @@ const item = names[Math.floor(Math.random()*names.length)]
 document.title = item[0];
 var link = document.createElement('link');
 link.rel = 'icon';
-link.href = item[1]
+link.href = `https://www.google.com/s2/favicons?domain=${item[1]}&sz=32`
 document.head.appendChild(link);
